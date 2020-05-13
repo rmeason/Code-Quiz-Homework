f4957646729b6b32a0678b83ca2c2b9c91793c5e
Richard Michael Eason <rme15a@acu.edu>
2020-05-12 19:28:57 -0500

    updated js with variable 'quiz'

---
+++ script.js
@@ -0,0 +1,29 @@
+
+
+//variable and values
+var quiz = {
+  "Using Git Bash, list all of the branches.": "$ git branch",
+  "HTML stands for: ": "Hypertext Markup Language",
+  "CSS stands for: ": "Cascading Style Sheets",
+  "Where inside which HTML element do we put the JavaScript?": "In a script tag",
+  "How can you add a comment in a JavaScript?": "//",
+
+};
+
+
+
+var input1 = "Using Git Bash, list all of the branches.";
+var input2 = "HTML stands for: ";
+var input3 = "CSS stands for: ";
+var input4 = "Where inside which HTML element do we put the JavaScript?";
+var input5 = "How can you add a comment in a JavaScript?";
+
+// How do I use input to get " "
+var index = quizParts.indexOf(input1, input2, input3, input4, input5);
+var valuesIndex = quizValues[index];
+
+//trying a foreach function
+
+
+
+//check gandolf activity
